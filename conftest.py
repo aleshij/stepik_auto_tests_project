a7d4cdcaf00c64a4aa07429951b9814c3fc90fae
aleshij <salexys@gmail.com>
2022-03-17 14:45:37 +0300

    Fix: call driver in conftest.py New: add new test

--- conftest.py
+++ conftest.py
@@ -9,23 +9,22 @@ def pytest_addoption(parser):
                      help='Choose language, default english')
 
 
-# useragent = UserAgent()
-# options.set_preference('general.useragent.override', useragent.firefox)
-
-options = webdriver.FirefoxOptions()
-
-options.set_preference('general.useragent.override',
-                       'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0')
-options.set_preference('dom.webdriver.enable', False)
-
-service = Service("D:\\Dropbox\\GitHub\\geckodriver.exe")
-
-# Запустить в фоне
-# options.headless = True
-
-
-@pytest.fixture(scope="function")
+@pytest.fixture
 def browser(request):
+
+    # useragent = UserAgent()
+    # options.set_preference('general.useragent.override', useragent.firefox)
+
+    options = webdriver.FirefoxOptions()
+
+    options.set_preference('general.useragent.override',
+                           'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0')
+    options.set_preference('dom.webdriver.enable', False)
+
+    service = Service("D:\\Dropbox\\GitHub\\geckodriver.exe")
+
+    # Запустить в фоне
+    # options.headless = True
 
     user_language = request.config.getoption('language')
     options.set_preference('intl.accept_languages', user_language)
